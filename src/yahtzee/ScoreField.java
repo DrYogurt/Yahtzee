57f4afc0ab96d1bf605ae28432fd1171d5406550
Robosax560 <ben.z.rubinstein@gmail.com>
2017-11-01 09:59:48 -0700

    ScoreField Completed (NOTTESTED)

--- src/yahtzee/ScoreField.java
+++ src/yahtzee/ScoreField.java
@@ -1,6 +1,7 @@
 package yahtzee;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 enum ScoreField { // Note: You can get the string by doing Field.____.name();
 		// Upper Fields
@@ -38,24 +39,97 @@ enum ScoreField { // Note: You can get the string by doing Field.____.name();
 		// Lower Field
 		THREEOFKIND 	((Die[] dice) -> {
 			// TODO: for all of these, Arrays.sort(dice) then make it check them for legitimacy
-			int sum=0;
-			for (Die d : dice) sum+=d.getValue();
-			return sum;
+			Arrays.sort(dice, new Comparator<Die>() {
+    	    	public int compare(Die a, Die b) {
+    	        	return Integer.compare(a.getValue(), b.getValue());
+    	    	}
+    		});
+    		
+    		for(int i = 2; i < dice.length; i++) {
+    			if(dice[i].getValue() == dice[i - 1].getValue() && dice[i].getValue() == dice[i - 2].getValue()) {
+    				int sum=0;
+					for (Die d : dice) sum+=d.getValue();
+					return sum;
+    			}
+    		}
+    		return 0;
 		}),
 		FOUROFKIND 		((Die[] dice) -> {
-			int sum=0;
-			for (Die d : dice) sum+=d.getValue();
-			return sum;
+			Arrays.sort(dice, new Comparator<Die>() {
+    	    	public int compare(Die a, Die b) {
+    	        	return Integer.compare(a.getValue(), b.getValue());
+    	    	}
+    		});
+    		
+    		for(int i = 3; i < dice.length; i++) {
+    			if(dice[i].getValue() == dice[i - 1].getValue() && dice[i].getValue() == dice[i - 2].getValue() && dice[i].getValue() == dice[i - 3].getValue()) {
+    				int sum=0;
+					for (Die d : dice) sum+=d.getValue();
+					return sum;
+    			}
+    		}
+    		return 0;
 		}),
-		FULLHOUSE 		((Die[] dice) -> {return 25;}),
-		SMALLSTRAIGHT	((Die[] dice) -> {return 30;}),
-		LARGESTRAIGHT 	((Die[] dice) -> {return 40;}),
+		FULLHOUSE 		((Die[] dice) -> {
+			Arrays.sort(dice, new Comparator<Die>() {
+    	    	public int compare(Die a, Die b) {
+    	        	return Integer.compare(a.getValue(), b.getValue());
+    	    	}
+    		});
+    		if((dice[0].getValue() == dice[1].getValue()
+    		&& dice[2].getValue() == dice[3].getValue()
+    		&& dice[2].getValue() == dice[4].getValue())
+    		||(dice[0].getValue() == dice[1].getValue()
+    		&& dice[0].getValue() == dice[2].getValue()
+    		&& dice[3].getValue() == dice[4].getValue()) {
+    			return 25;
+    		}
+    		return 0;
+		}),
+		SMALLSTRAIGHT	((Die[] dice) -> {
+			Arrays.sort(dice, new Comparator<Die>() {
+    	    	public int compare(Die a, Die b) {
+    	        	return Integer.compare(a.getValue(), b.getValue());
+    	    	}
+    		});
+			for (int i = 0; i < dice.length -1; i++) {
+				if(!(dice[0] == (dice[i] - i))) {
+					return 0;
+				}
+			}
+			for (int i = 1; i < dice.length; i++) {
+				if(!(dice[0] == (dice[i] - (i - 1))) {
+					return 0;
+				}
+			}
+			return 30;
+		}),
+		LARGESTRAIGHT 	((Die[] dice) -> {
+			Arrays.sort(dice, new Comparator<Die>() {
+    	    	public int compare(Die a, Die b) {
+    	        	return Integer.compare(a.getValue(), b.getValue());
+    	    	}
+    		});
+			for (int i = 0; i < dice.length; i++) {
+				if(!(dice[0] == (dice[i] - (i - 1))) {
+					return 0;
+				}
+			}
+			return 40;
+		}),
 		CHANCE 			((Die[] dice) -> {
 			int sum=0;
 			for (Die d : dice) sum+=d.getValue();
 			return sum;
 		}),
-		YAHTZEE 		((Die[] dice) -> {return 50;});
+		YAHTZEE 		((Die[] dice) -> {
+			Arrays.sort(dice, new Comparator<Die>() {
+    	    	public int compare(Die a, Die b) {
+    	        	return Integer.compare(a.getValue(), b.getValue());
+    	    	}
+    		});
+			return (dice[0] == dice[dice.length - 1])?(50):(0);
+		});
 
 		// Class Info
 
